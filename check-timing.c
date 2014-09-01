f977bff80c20eb1c1cce4f72a560d27e2604ec9f
Ryan Dwyer <ryanpdwyer@gmail.com>
2014-08-31 22:38:41 -0400

    Added minunit example to the file

--- check-timing.c
+++ check-timing.c
@@ -1,13 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <tgmath.h>
-#include "check-timing.h"
+// check-timing.c
+// Ryan Dwyer
+// 2014-08-30
+
+// This file implements a structure, TimingParameter, defined in check-timing.h.
+// It is designed to allow for easy conversion between sampling frequency (fs),
+// time between data points (dt), number of samples (N),
+// and total sampling time (T) in LabView.
+
+// The structure also defines
+
+// TODO:
+// Define an enum to deal with statuses.
+// Use minunit to do some unittests.
+
 
 // We will compile this as a dll to use in Labview. See
 // https://cygwin.com/cygwin-ug-net/dll.html
 // Steps:
 // gcc -c check-timing.c
 // gcc -shared -o check-timing.dll check-timing.o
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <tgmath.h>
+#include "check-timing.h"
+#include "minunit.h"
 
 // Initialize a TimingParameter on the heap.
 void* TP_init(double fs, double dt, double N, double T) {
@@ -16,10 +34,20 @@ void* TP_init(double fs, double dt, double N, double T) {
     tp->dt = dt;
     tp->N = N;
     tp->T = T;
-    tp->eps = 0.1; // Default to 0.1 for eps
+    tp->eps = 0.1; // Default to 0.1 for eps; used to determine if a
+                   // TimingParameter is internally consistant.
     return tp;
 }
 
+MU_TEST(test_TP_init) {
+    TimingParameter* tp = TP_init(25.0, 0.04, 100, 4);
+    mu_assert_double_eq(25.0, tp->fs);
+    mu_assert_double_eq(0.04, tp->dt);
+    mu_assert_double_eq(100, tp->N);
+    mu_assert_double_eq(4, tp->T);
+}
+
+// Free a timing parameter created on the heap.
 void TP_destroy(TimingParameter *tp) {
     free(tp);
 }
@@ -34,11 +62,16 @@ int fs_dt_consistent(TimingParameter *tp) {
         // Both defined; check that they are with the tolerance eps
         float fs_dt = 1.0 / tp->dt;
         if (fabs(fs_dt - tp->dt) <= tp->eps) {
-            tp->dt = 1 / tp->fs;
+            // Make sure the timing parameters are actually consistent
+            // Since fs is what is sent to the DAQ, we'll define dt in terms
+            // of fs.
+            tp->dt = 1.0 / tp->fs; 
         }
         else {
-            status = 1; // Timing parameters don't match
-            tp->dt = 1 / tp->fs;
+             // Timing parameters don't match. Leave the situation to be
+            // cleaned up by the next function, or define dt in terms of fs?.
+            status = 1;
+            tp->dt = 1.0 / tp->fs;
         }
     }
     else if (fs_defined && !dt_defined) {
@@ -50,7 +83,8 @@ int fs_dt_consistent(TimingParameter *tp) {
         tp->fs = 1.0 / tp->dt;
     }
     else if (!fs_defined && !dt_defined) {
-        // Neither defined. Exit with status 2; could be an error, could not.
+        // Neither defined. Exit with status 2, and let N_T consistent deal
+        // with this.
         status = 2;
     }
     return status;
@@ -60,6 +94,8 @@ int fs_dt_consistent(TimingParameter *tp) {
 // structure consistent.
 
 int N_T_consistent(TimingParameter *tp) {
+    // This function is designed to be called after fs_dt_consistent, and will
+    // assign the rest of the unassigned parameters.
     int status = 0;
     int N_defined = tp->N > 0;
     int T_defined = tp->T > 0;
@@ -68,17 +104,20 @@ int N_T_consistent(TimingParameter *tp) {
 
     if (N_defined && T_defined) {
         if (!fs_defined && !dt_defined) {
-            // We can just define all the variables here then.
+            // This is a normal case; define fs in terms of N and T, then
+            // dt in terms of fs.
             tp->fs = tp->N / tp->T;
             tp->dt = 1.0 / tp->fs;
         }
         else if (fs_defined && dt_defined) {
             // This is complicated, but things are all defined, so just raise
-            // a status and get out.
+            // a status and get out. Should probably have a function to check 
+            // the cluster's consistency here.
             status = 3;
         }
         else {
-            // Things are weird. Raise a status.
+            // The structure should not get into this state. Just raise a
+            // status and get out.
             status = 4;
         }
     } else if (N_defined && !T_defined) {
@@ -136,6 +175,10 @@ int TP_check(TimingParameter* tp) {
     return (status || status2);
 }
 
+MU_TEST_SUITE(test_suite) {
+    MU_RUN_TEST(test_TP_init);
+}
+
 int main(int argc, char const *argv[])
 {
 
@@ -164,5 +207,9 @@ int main(int argc, char const *argv[])
     TimingParameter* tp_neither = TP_init(0, 0, 10, 0);
     check_tp_case(tp_neither, "Neither defined");
     TP_destroy(tp_neither);
+
+    MU_RUN_SUITE(test_suite);
+    MU_REPORT();
+
     return 0;
 }
